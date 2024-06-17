b1cfd7838d020b58e9df671ee15cd20e9106486c
david-chen-sm <hahasmchen@gmail.com>
2024-06-16 23:22:42 -0400

    Add files via upload

--- src/main/java/com/hotel/revervation/controller/RoomController.java
+++ src/main/java/com/hotel/revervation/controller/RoomController.java
@@ -1,8 +1,11 @@
 package com.hotel.revervation.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,31 +19,40 @@ import com.hotel.revervation.dto.Room;
 import com.hotel.revervation.service.RoomService;
 
 /**
- * This project is a demonstration of skills, understanding, and the use of Spring Boot functionalities.
- * Therefore, the return types might not strictly adhere to industry standards. It is intended to demonstrate
- * the use of exceptions, DTO objects, and ResponseEntity as return types to interact with the frontend
- * (e.g., Postman application).
+ * This project serves as a demonstration of skills, understanding, and the practical use of Spring Boot functionalities.
+ * The return types in this project may not strictly adhere to industry standards. Instead, the focus is on showcasing the
+ * implementation of exceptions handling, DTO objects, and ResponseEntity as return types for interaction with front-end
+ * applications (e.g., Postman). More detailed Javadoc comments are provided selectively for complex functions due to time constraints.
  */
 
 @RestController
 public class RoomController {
 	
 	@Autowired
-	RoomService roomService;
+	private RoomService roomService;
 	
 	@PostMapping("/room")
-	public Room saveRoom(@RequestBody Room room) {
-		return roomService.saveRoom(room);
+	public ResponseEntity<Room> saveRoom(@RequestBody Room room) {
+		Room savedRoom = roomService.saveRoom(room);
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedRoom);
 	}
 	
 	@GetMapping("/room")
-	public List<Room> getAllRooms(){
-		return roomService.getAllRooms();
+	public ResponseEntity<List<Room>> getAllRooms(){
+		List<Room> dbRooms = roomService.getAllRooms(); 
+		return ResponseEntity.ok(dbRooms);
 	}
 	
 	@GetMapping("/room/{id}")
-	public Room getRoomById(@PathVariable int id) {
-		return roomService.getRoomById(id);
+	public ResponseEntity<Room> getRoomById(@PathVariable int id) {
+		Room dbRoom = null;
+		try {
+			dbRoom = roomService.getRoomById(id);
+		} catch (NoSuchElementException e) {
+			// No room is found with roomId
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(dbRoom) ;
 	}
 	
 	@DeleteMapping("/room/{id}")
@@ -49,8 +61,20 @@ public class RoomController {
 	}
 	
 	@PutMapping("/room")
-	public Room updateRoom(@RequestBody Room room) {
-		return roomService.updateRoom(room);
+	public ResponseEntity<Room> updateRoom(@RequestBody Room room) {
+		Room dbRoom = null;
+		try {
+			dbRoom = roomService.updateRoom(room);
+		} catch (NoSuchElementException e) {
+			// No room is found with roomId
+			return ResponseEntity.notFound().build();
+		}
+		
+		if (Objects.isNull(dbRoom)) {
+			// no update was made
+			return ResponseEntity.badRequest().build();
+		}
+		return ResponseEntity.ok(dbRoom);
 	}
 	
 	@PostMapping("/room/{userId}/{roomId}")
