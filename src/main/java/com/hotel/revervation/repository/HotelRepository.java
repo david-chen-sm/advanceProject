957d4ae1c1f53e9404a2c704a9d418029f6bb199
david-chen-sm <hahasmchen@gmail.com>
2024-06-17 22:39:07 -0400

    Delete src directory

--- src/main/java/com/hotel/revervation/repository/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hotel.revervation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hotel.revervation.dto.Hotel;
-
-public interface HotelRepository extends JpaRepository<Hotel, Integer>{
-	
-}
--- src/main/java/com/hotel/revervation/repository/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hotel.revervation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hotel.revervation.dto.Room;
-
-public interface RoomRepository extends JpaRepository<Room, Integer>{
-	
-}
